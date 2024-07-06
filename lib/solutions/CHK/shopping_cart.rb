c46d90d3c15938e38206801dcba294343a090b46
sbx_user1051 <sbx_user1051@169.254.105.181>
2024-07-06 23:14:20 +0000

    Sat Jul 06 23:14:20 UTC 2024

--- lib/solutions/CHK/shopping_cart.rb
+++ lib/solutions/CHK/shopping_cart.rb
@@ -33,13 +33,13 @@ class ShoppingCart
       items_in_group = skus.chars.map { |sku| quantity_for sku }.sum
       batches = items_in_group / batch_size
       add_items(skus, batches)
-      next if batches == 0
 
       skus.chars.each do |sku|
-        break if items_in_group == 0
+        break if batches == 0
         being_batched = [items_in_group, quantity_for(sku)].min
         items_in_group -= being_batched
         add_items(sku, -being_batched)
+        puts "Remove #{being_batched} instances of #{sku}"
       end
     end
   end
@@ -57,3 +57,4 @@ class ShoppingCart
     free_items
   end
 end
+
