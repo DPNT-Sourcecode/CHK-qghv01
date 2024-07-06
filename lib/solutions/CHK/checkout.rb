43fcc86a90279553cff0b8f8fbd98946f251b4a6
sbx_user1051 <sbx_user1051@169.254.105.181>
2024-07-06 22:56:20 +0000

    Sat Jul 06 22:56:20 UTC 2024

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -69,13 +69,11 @@ class Checkout
 
   def price_for_multiple(item_sku, count)
     sum = 0
+    pp [item_sku, count]
 
     available_offers(item_sku).each do |offer|
       batch_size, batch_price = offer
       batches = count / batch_size
-      if batches > 0
-        binding.irb
-      end
       sum += batch_price * batches
       count -= batches * batch_size
     end
@@ -119,6 +117,7 @@ class Checkout
     volume_offers[group_id] << [batch_size, price]
   end
 end
+
 
 
 
